493018ef9ae35ecf3cd61b87605a9e4623840b49
ringotc <ringotc@outlook.com>
2025-01-24 10:39:22 -0800

    fix: syntax error

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python main.py train --dataset comta --crossval --model_type dkt-sem --model_name dkt-sem_comta
 
-python main.py train --dataset comta --crossval --model_type dkt-sem --model_name dkt-sem-rdrop_comta
+python main.py train --dataset comta --crossval --model_type dkt-sem-rdrop --model_name dkt-sem-rdrop_comta
